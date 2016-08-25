20b60d1c69689c9949721863f3c20c8ee665a09c
Beth Latronico <beth@berkeley.edu>
2016-08-25 10:09:27 -0400

    Do not print the test title as a classname attribute in <testcase>s. Jenkins interprets the classname as the top-level package and therefore creates a separate package for each test name.

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ MochaJUnitReporter.prototype.getTestcaseData = function(test, err) {
       _attr: {
         name: test.fullTitle(),
         time: (typeof test.duration === 'undefined') ? 0 : test.duration / 1000,
-        classname: test.title
+        //classname: test.title
       }
     }]
   };
